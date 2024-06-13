d3397fd4c43fc8cb8f3540faf681087c2ac0c349
Aniket <156108186+aniketsrinivasan@users.noreply.github.com>
2024-06-13 20:56:08 +0100

    Restructured directories.

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from .master_model import MasterModel
-from .model_XGBoostCV_v1 import XGBoostCV_v1
-from .model_XGBoostTTV_v1 import XGBoostTTV_v1
+from .xgboost import XGBoostCV_v1
+from .xgboost import XGBoostTTV_v1
---
+++ models/xgboost/__init__.py
@@ -0,0 +1,2 @@
+from .model_XGBoostCV_v1 import XGBoostCV_v1
+from .model_XGBoostTTV_v1 import XGBoostTTV_v1
