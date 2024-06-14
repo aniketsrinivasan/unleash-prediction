28794b5b0215130d5b061e37031740c36fcdfea2
Aniket <156108186+aniketsrinivasan@users.noreply.github.com>
2024-06-14 19:05:02 +0100

    Updated commit.

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-from utils import (data_read_csv, data_datetime_conversion, data_datetime_sort,
-                   data_datetime_create_features, data_create_lags, data_split_features_labels,
-                   data_split_train_test_valid, data_create_future)
-from models import MasterModel
-from models import (XGBoostCV_v1, XGBoostTTV_v1)
+from models import *
 from utils import TimeSeries
 
 
 def main():
+    # To-do:
+    #   * add method to vary split_ratio based on future_window_size (so that valid is same size)
+    #   * add XGBoostCV_v2 (another method of cross-validation) using sk-learn (?)
+    #   * add method to master_model to test accuracy using validation set:
+    #       requires that validation >= future_window_size
+    #   * add visualizations to graphs
+    #   * add function in dataset_utils to take the predictions and original dataset, and merge them
+    #       this should have columns datetime_name and value_name, and a new masking column "is_future"
+    #   * experiment with the following models (hopefully on other datasets, too):
+    #       - other XGBoost variants and hyperparameters
+    #       - LSTM (would need to build using PyTorch)
+    #       - transformer? (this doesn't look promising though)
+    #       - fourier analytic methods (i.e. wavelet transform?)
+    #           the idea is to try encoding the frequencies into the training data directly
+    #           most likely would require modifying feature creation (to include fourier)
+
     time_series = TimeSeries(csv_path="/Users/aniket/PycharmProjects/unleashPredictions/energy_data.csv",
                              datetime_name="Datetime",
                              datetime_format="%Y-%m-%d %H:%M:%S",
                              value_name="PJMW_MW",
-                             verbose=True)
+                             verbose=False)
     time_series.prepare_from_scratch(future_window_size=24*7*4,
                                      future_step_size="1h",
-                                     split_ratio=[0.7, 0.2, 0.1],
+                                     split_ratio=[0.9, 0.05, 0.05],
                                      kwargs_features=None,
                                      kwargs_lags=None)
     print(time_series)
 
-    # model = XGBoostCV_v1(time_series=time_series)
-    # model.train()
-    # result = model.predict()
-    # print(result)
+    model = XGBoostCV_v1(time_series=time_series)
+    model.train()
+    result = model.predict()
+    print(result[:10])
+    loss, loss_df = validation_loss(model)
 
     model = XGBoostTTV_v1(time_series=time_series)
     model.train()
     result = model.predict()
-    print(result)
+    print(result[:10])
+    loss, loss_df = validation_loss(model)
 
 
 if __name__ == '__main__':
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .master_model import MasterModel
+from .model_tester import validation_loss
 from .xgboost import XGBoostCV_v1
 from .xgboost import XGBoostTTV_v1
