d3df39015ca9cabd40ecacccf2c8fa7d3b4156a8
Aniket <156108186+aniketsrinivasan@users.noreply.github.com>
2024-06-17 15:06:37 +0100

    Updated MasterModel framework to control Model(s).

--- models/master_model.py
+++ models/master_model.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import numpy as np
-import xgboost as xgb
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from matplotlib import pyplot as plt
-from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import mean_squared_error
+from .xgboost import *
+from .model_tester import validation_loss
 from utils import TimeSeries
 
 
@@ -18,18 +14,77 @@ class MasterModel:
     #   time_series.value_name:         the Target to predict.
     #   time_series.df_split_<>:        splits used for training, testing and validation (depending on model).
     #   time_series.future_<>:          dataset onto which the model predicts (depending on model).
-    def __init__(self, time_series: TimeSeries, model: str):
+    def __init__(self, time_series: TimeSeries, model_name: str, read_stub=False, stub_path=None):
         """
         MasterModel uses a TimeSeries, and a provided model, to train and forecast.
 
         :param time_series:     TimeSeries object [must be fully prepared, use prepare_from_scratch()].
-        :param model:           the model used, as a str; currently supports: "...".
+        :param model_name:      the model used, as a str; currently supports: "...".
         """
         # Storing the time_series and model_name:
         self.time_series = time_series
-        self.model_name = model
+        self.model_name = model_name
 
         # Creating a list of features (as column names) for the dataset:
         self.features = time_series.features + time_series.lags
         # Target to predict (as a column name):
         self.target = time_series.value_name
+
+        # Storing information about the model:
+        self.model = None                           # the actual Model object for this instance
+        self.model_kwargs = None                    # storing any special arguments used
+        self.model_validation_loss = None           # validation loss
+        self.model_validation_dataframe = None      # df_merged from validation loss
+
+    def model_create(self):
+        """
+        Initializes this model's Model and stores it in self.model.
+
+        :return:    None.
+        """
+        if self.model_name == "XGBoostCV_v1":
+            self.model = XGBoostCV_v1(self.time_series)
+        elif self.model_name == "XGBoostTTV_v1":
+            self.model = XGBoostTTV_v1(self.time_series)
+        else:
+            raise ValueError("Model not found. Check whether model_name is correct.")
+
+        return
+
+    def model_train(self):
+        """
+        Trains the model stored in self.model. Stores the trained model back in self.model.
+        Requires that self.model_create() has been run.
+
+        :return:        None.
+        """
+        return self.model.train()
+
+    def model_predict(self, custom_df=None):
+        """
+        Predicts using the Model's predict() method. Requires that self.model_train() has been run.
+
+        :param custom_df:       predicting on a custom DataFrame (optional).
+        :return:                None.
+        """
+        return self.model.predict(custom_df)
+
+    def model_get_validation_loss(self, loss_function="mean_squared_error", verbose=True):
+        self.model_validation_loss, self.model_validation_dataframe = validation_loss(self.model,
+                                                                                      loss_function=loss_function,
+                                                                                      verbose=verbose)
+        return self.model_validation_loss, self.model_validation_dataframe
+
+    def model_get_dict(self):
+        # {model_name: {"model": Model(trained),
+        #               #                 "validation_loss": 103934,
+        #               #                 "hyperparameters": {<dictionary of hyperparameters>},
+        #               #                 "predictions": predictions (as a list)}
+        model_dict = dict()
+        model_dict["model"] = self.model
+        model_dict["hyperparameters"] = self.model_kwargs
+        model_dict["validation_loss"] = self.model_validation_loss
+        model_dict["validation_df"] = self.model_validation_dataframe
+        return model_dict
+
+
